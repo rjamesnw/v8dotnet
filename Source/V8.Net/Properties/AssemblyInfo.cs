3bb68d97ab51acb5af0f2af949c0ad524bfecfda
James <contactme@jameswilkins.net>
2013-09-10 01:27:27 -0400

    First release checkin under Git source control! :)

--- Source/V8.Net/Properties/AssemblyInfo.cs
+++ Source/V8.Net/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.2013.9")]
-[assembly: AssemblyFileVersion("1.3.14.29")]
+[assembly: AssemblyFileVersion("1.3.14.39")]
