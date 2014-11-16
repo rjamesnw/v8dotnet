f172b03384f1f485e7028483268dfc3327c7adb2
chrisber <christian@chrisber.net>
2014-11-16 11:17:58 +0100

    update script

--- build_V8_Net.sh
+++ build_V8_Net.sh
@@ -14,8 +14,10 @@ currentDir=`pwd`
 	printf '\e[1;34m%-6s\e[m \n' "Building V8 "
 	printf '\e[1;34m%-6s\e[m \n' "Version 3.30.13 (based on bleeding_edge revision r24708) "
 	printf '\e[1;34m%-6s\e[m \n' "commit 9a58807030208121b7e9f01aca2b932eb52e249f "
-	make builddeps
-	make native library=shared 
+	printf '\e[1;34m%-6s\e[m \n' "make builddeps -j ${JOBSV8=$1}"
+	make builddeps -j ${JOBSV8=$1}
+	printf '\e[1;34m%-6s\e[m \n' "make native library=shared -j ${JOBSV8=$1}"
+	make native library=shared -j ${JOBSV8=$1}
 }
 
  buildV8Proxy (){
@@ -27,12 +29,12 @@ currentDir=`pwd`
 
 	cd Source/V8.NET-Proxy/
 	printf '\e[1;34m%-6s\e[m \n' "Build V8DotNet Proxy"
-	ls | grep cpp | awk -F. '{ system("g++  -std=c++11   -w -fpermissive -fPIC  -lstdc++ -Wl,--gc-sections   -c -IV8/ -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/ "$1".cpp -o out/"$1".o ") }'
+	ls | grep cpp | awk -F. '{ system("g++  -std=c++11 -g  -w -fpermissive -fPIC  -lstdc++ -Wl,--gc-sections   -c -IV8/ -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/ "$1".cpp -o out/"$1".o ") }'
 	cd out
 	cp ../V8/out/native/lib.target/libicui18n.so .
 	cp ../V8/out/native/lib.target/libicuuc.so .
 	cp ../V8/out/native/lib.target/libv8.so .
-	g++ -Wall -std=c++11 -shared  -fPIC -I../ -I../V8/ -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/   -Wl,-soname,libV8_Net_Proxy.so  -o libV8_Net_Proxy.so *.o ../V8/out/native/obj.host/testing/libgtest.a ../V8/out/native/obj.target/testing/libgmock.a ../V8/out/native/obj.target/testing/libgtest.a ../V8/out/native/obj.target/third_party/icu/libicudata.a ../V8/out/native/obj.target/tools/gyp/libv8_base.a ../V8/out/native/obj.target/tools/gyp/libv8_libbase.a ../V8/out/native/obj.target/tools/gyp/libv8_libplatform.a ../V8/out/native/obj.target/tools/gyp/libv8_nosnapshot.a ../V8/out/native/obj.target/tools/gyp/libv8_snapshot.a  -Wl,-rpath,. -L. -L../  -lpthread  -lstdc++ -licui18n -licuuc -lv8 -lglib-2.0 -lrt  -Wl,--verbose
+	g++ -Wall -std=c++11 -shared -g -fPIC -I../ -I../V8/ -I/usr/include/glib-2.0/ -I/usr/lib/x86_64-linux-gnu/glib-2.0/include/   -Wl,-soname,libV8_Net_Proxy.so  -o libV8_Net_Proxy.so *.o ../V8/out/native/obj.host/testing/libgtest.a ../V8/out/native/obj.target/testing/libgmock.a ../V8/out/native/obj.target/testing/libgtest.a ../V8/out/native/obj.target/third_party/icu/libicudata.a ../V8/out/native/obj.target/tools/gyp/libv8_base.a ../V8/out/native/obj.target/tools/gyp/libv8_libbase.a ../V8/out/native/obj.target/tools/gyp/libv8_libplatform.a ../V8/out/native/obj.target/tools/gyp/libv8_nosnapshot.a ../V8/out/native/obj.target/tools/gyp/libv8_snapshot.a  -Wl,-rpath,. -L. -L../  -lpthread  -lstdc++ -licui18n -licuuc -lv8 -lglib-2.0 -lrt  -Wl,--verbose
 	cp *.so ../../../BuildOutput/Debug
 	cp *.so ../../../BuildOutput/Release
 }
@@ -55,15 +57,18 @@ currentDir=`pwd`
 }
 
 
-USAGE="$(basename "$0") [-h] [-l] [-d] [-v8] -- program to build V8DotNet\n\n
+USAGE="$(basename "$0") [-h] [-l] [-d jobs] [-v8 jobs] -- program to build V8DotNet\n\n
 
-Use: [$(basename "$0") --default] to build all\n\n
+Use: [$(basename "$0") --default 4] to build all\n\n
 
 where:\n
     -h,  --help:     \t Show this help text\n
     -l,  --lib:      \t libV8_Net_Proxy.so only\n
-    -v8, --v8:       \t Build Google V8 only\n
-    -d, --default:   Build V8DotNet with extern Google V8\n"
+    -v8, --v8:       \t Build Google V8 only \n
+    -d, --default:   Build V8DotNet with extern Google V8\n\n
+
+    param jobs: specifies the number of parallel build processes. Set it (roughly) to the number of CPU cores your machine has. 
+    The GYP/make based V8 build also supports distcc, so you can compile with -j100 or so, provided you have enough machines around." 
     
 if [ $# == 0 ] ; then
     echo -e $USAGE
@@ -82,12 +87,14 @@ case $key in
     shift
     ;;
     -d|--default)
+	JOBSV8=$1
     buildV8
     buildV8Proxy
     buildV8DotNet
     shift
     ;;
     -v8|--v8)
+	JOBSV8=$1
     buildV8
     shift
     ;;
