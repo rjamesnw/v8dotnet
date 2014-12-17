00ce53991bb318b517e1acbb11729e23a2149f03
James <contactme@jameswilkins.net>
2014-12-17 18:20:49 -0500

    Updated binaries

--- Source/V8.Net/Properties/AssemblyInfo.cs
+++ Source/V8.Net/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.4.2014.12")]
-[assembly: AssemblyFileVersion("1.4.28.16")]
+[assembly: AssemblyFileVersion("1.4.28.22")]
