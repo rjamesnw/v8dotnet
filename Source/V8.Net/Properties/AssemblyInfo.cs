a1dda0630580bb45516d63ed16570d244502252c
James <contactme@jameswilkins.net>
2015-01-07 12:45:14 -0500

    Just moving files around.

--- Source/V8.Net/Properties/AssemblyInfo.cs
+++ Source/V8.Net/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.4.2015.1")]
-[assembly: AssemblyFileVersion("1.4.30.26")]
+[assembly: AssemblyFileVersion("1.4.30.32")]
