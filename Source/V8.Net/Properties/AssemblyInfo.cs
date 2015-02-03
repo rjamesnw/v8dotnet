b78d4c3f34ed425b5259dbce5098049df82e19d0
James <contactme@jameswilkins.net>
2015-02-02 20:03:24 -0500

    Updated test console project (debug build path was wrong after moving the project files), and tested pull request for merge fix regarding OverflowException error in marshal testing on Windows 8 systems.

--- Source/V8.Net/Properties/AssemblyInfo.cs
+++ Source/V8.Net/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.2015.2")]
-[assembly: AssemblyFileVersion("1.5.2.15")]
+[assembly: AssemblyFileVersion("1.5.2.41")]
