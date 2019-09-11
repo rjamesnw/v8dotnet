df8eb0f2387147abcc69e356764b88fb11cbb225
jwilkin2 <jwilkin2@NA.HOLCIM.NET>
2019-09-11 17:07:09 -0400

    Dev release v1.1.8.

--- Source/V8.Net/Properties/AssemblyInfo.cs
+++ Source/V8.Net/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.2019.9")]
-[assembly: AssemblyFileVersion("1.5.54.10")]
+[assembly: AssemblyFileVersion("1.5.54.11")]
